6b83a74b2c3f4f0fc0b435166bd8e5ffa705f63e
Quang Pham <test@quangpham.com>
2013-05-31 12:08:56 +0300

    Auto binding child

--- SayduckNdata/UIHelper.cs
+++ SayduckNdata/UIHelper.cs
@@ -38,12 +38,21 @@ public class UIHelper : MonoBehaviour {
 	public void addChildView ()
 	{
 		GameObject mainViewContainer = GameObject.Find("MainViewContainer");
-		GameObject view = (GameObject)Instantiate(Resources.Load("_Ndata/SecondListView"));
-		
 		mainViewContainer.GetComponent<MainViewContainerController>().OnAddSubView();
+		int childLevel = mainViewContainer.GetComponent<MainViewContainerController>().MenuLevelCount;
 		
+		GameObject view = (GameObject)Instantiate(Resources.Load("_Ndata/ChildListView"));
+		view.name = "Child_" + childLevel.ToString();
 		view.transform.parent = mainViewContainer.transform;
 		view.transform.localScale = new Vector3 (1,1,1);
-		view.transform.localPosition = new Vector3 (Screen.width*mainViewContainer.GetComponent<MainViewContainerController>().MenuLevelCount, view.transform.localPosition.y, view.transform.localPosition.z);
+		view.transform.localPosition = new Vector3 (Screen.width*childLevel, view.transform.localPosition.y, view.transform.localPosition.z);
+		
+		string itemSourceBindingString = "RootUIMenuItem";
+		for (int i = 0; i<childLevel; i++)
+		{
+			itemSourceBindingString += ".SelectedItem.SubMenuItems";
+		}
+		NguiItemsSourceBinding itemsSourceBinding = GameObject.Find("Child_" + childLevel.ToString() + "/PanelsParent/3.ListViewPanel/DraggablePanel/UIGrid").GetComponent<NguiItemsSourceBinding>();
+		itemsSourceBinding.Path = itemSourceBindingString;
 	}
 }
